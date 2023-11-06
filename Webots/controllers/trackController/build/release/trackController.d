36377d70cedbc2f40edd5cab5e430a885c6ae003
Pyroghost0 <89740009+Pyroghost0@users.noreply.github.com>
2023-11-06 10:18:17 -0600

    Delete Unnecessary Files

--- Webots/controllers/trackController/build/release/trackController.d
+++
@@ -1,6 +0,0 @@
-build/release/trackController.o: trackController.cpp \
- C:\Program\ Files\Webots/include/controller/cpp/webots/Robot.hpp \
- C:\Program\ Files\Webots/include/controller/cpp/webots/Motor.hpp \
- C:\Program\ Files\Webots/include/controller/cpp/webots/Device.hpp \
- C:/Program\ Files/Webots/include/controller/c/webots/types.h \
- C:\Program\ Files\Webots/include/controller/cpp/webots/DistanceSensor.hpp
